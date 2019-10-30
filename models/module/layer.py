c7f7866372658dd44e1a81ec9dc4d4f886997b76
jjeamin <woalsdl500@naver.com>
2019-10-31 01:10:50 +0900

    Update layer module

---
+++ models/module/layer.py
@@ -0,0 +1,28 @@
+import torch.nn as nn
+
+class conv_bn_relu(nn.Module):
+    def __init__(self,in_planes,planes,kernel_size,stride=1,padding=0,bias=True):
+        super(conv_bn_relu,self).__init__()
+
+        self.C = nn.Conv2d(in_planes,planes,
+                           stride=stride,kernel_size=kernel_size,
+                           padding=padding,bias=bias)
+        self.BN = nn.BatchNorm2d(planes)
+        self.A = nn.ReLU()
+
+    def forward(self, x):
+        return self.A(self.BN(self.C(x)))
+
+
+class conv_bn(nn.Module):
+    def __init__(self,in_planes,planes,kernel_size,stride=1,padding=0,bias=True):
+        super(conv_bn,self).__init__()
+
+        self.C = nn.Conv2d(in_planes, planes,
+                           stride=stride,kernel_size=kernel_size,
+                           padding=padding,bias=bias)
+        self.BN = nn.BatchNorm2d(planes)
+        self.A = nn.ReLU()
+
+    def forward(self, x):
+        return self.A(self.BN(self.C(x)))
