b7b26c8401521038efa19238563147471ab83796
jjeamin <woalsdl500@naver.com>
2019-11-04 17:49:44 +0900

    Update tensor image save

--- utils/tester.py
+++ utils/tester.py
@@ -1,11 +1,23 @@
 import torch
+import skimage.io as io
+import matplotlib.pyplot as plt
 from torchsummary import summary
 
 class model_test:
-    def __init__(self,model):
+    def __init__(self, model):
         self.model = model
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu") # PyTorch v0.4.0
 
     def summary(self,input):
         net = self.model.to(self.device)
         summary(net, input)
+
+def tensor_image_save(image, file_name='test'):
+    '''
+    :param image: (tensor) cpu image
+    :return: (file) save image
+    '''
+
+    io.imshow(image.permute(1, 2, 0).numpy())
+    plt.savefig(file_name)
+    print('Finish image save testing')
